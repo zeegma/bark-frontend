9948a7cce1a517347d149d222d11e0580b385881
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-04-05 21:41:58 +0800

    Modified sample app to use tailwind [sample]

--- src/App.svelte
+++ src/App.svelte
@@ -4,44 +4,42 @@
   import Counter from './lib/Counter.svelte'
 </script>
 
-<main>
-  <div>
+<main class="text-center p-4">
+  <div class="flex justify-center items-center">
     <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
+      <img
+        src={viteLogo}
+        alt="Vite Logo"
+        class="h-24 p-6 transition-filter duration-300 hover:drop-shadow-[0_0_2em_#646cffaa]"
+      />
     </a>
     <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+      <img
+        src={svelteLogo}
+        alt="Svelte Logo"
+        class="h-24 p-6 transition-filter duration-300 hover:drop-shadow-[0_0_2em_#ff3e00aa]"
+      />
     </a>
   </div>
-  <h1>Vite + Svelte</h1>
 
-  <div class="card">
+  <h1 class="text-4xl font-bold my-4">Test main app w/ tailwind (Ugly AF)</h1>
+
+  <div class="my-6 p-6 bg-white rounded-xl shadow-sm inline-block">
     <Counter />
   </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  <p class="my-4">
+    Check out
+    <a
+      href="https://github.com/sveltejs/kit#readme"
+      target="_blank"
+      rel="noreferrer"
+      class="text-blue-500 underline hover:text-blue-700"
+    >
+      SvelteKit
+    </a>
+    , the official Svelte app framework powered by Vite!
   </p>
 
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <p class="text-gray-500">Click on the Vite and Svelte logos to learn more</p>
 </main>
-
-<style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
-</style>
