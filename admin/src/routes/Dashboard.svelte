5df3495ede0dcc89a6bdf9b964ab97b113ebc408
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-04-10 21:03:38 +0800

    Created pages for the admin side

---
+++ admin/src/routes/Dashboard.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+</script>
+
+<div class="bg-gray-100 p-8 rounded-lg max-w-md mx-auto mt-10 shadow-md">
+  <h1 class="text-3xl font-bold mb-6 text-green-600">Dashboard</h1>
+  <p class="text-gray-700 mb-4">Dashboard test</p>
+</div>
---
+++ admin/src/routes/Fallback.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+</script>
+
+<div class="bg-gray-100 p-8 rounded-lg max-w-md mx-auto mt-10 shadow-md">
+  <h1 class="text-3xl font-bold mb-6 text-purple-600">404</h1>
+  <p class="text-gray-700 mb-4">Page not found test</p>
+</div>
