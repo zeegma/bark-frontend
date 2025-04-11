203e64c00965006052d58ebfb6b655043b5b8079
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-04-11 13:39:17 +0800

    Created layout & added navbar here

---
+++ admin/src/components/layout/Layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import Navbar from "./Navbar.svelte";
+</script>
+
+<div class="flex min-h-screen">
+  <Navbar />
+  <main class="flex-1 overflow-auto p-4">
+    <slot />
+  </main>
+</div>
