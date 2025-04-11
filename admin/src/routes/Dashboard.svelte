11b3d994396c383a40881e99e67f0bcee09efbc2
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-04-11 13:40:22 +0800

    Imported layout component on all pages

--- admin/src/routes/Dashboard.svelte
+++ admin/src/routes/Dashboard.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+  import Layout from "../components/layout/Layout.svelte";
 </script>
 
-<div class="bg-gray-100 p-8 rounded-lg max-w-md mx-auto mt-10 shadow-md">
-  <h1 class="text-3xl font-bold mb-6 text-green-600">Dashboard</h1>
-  <p class="text-gray-700 mb-4">Dashboard test</p>
-</div>
+<Layout>
+  <div class="p-4">
+    <h1 class="text-2xl font-semibold">Dashboard</h1>
+  </div>
+</Layout>
