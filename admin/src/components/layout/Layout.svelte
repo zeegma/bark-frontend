346eb5274f82f4ed1090dbfb4a9b45fa847ee51c
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-04-11 22:40:23 +0800

    Added title on layout to share

--- admin/src/components/layout/Layout.svelte
+++ admin/src/components/layout/Layout.svelte
@@ -3,15 +3,24 @@
   import Searchbar from "../common/Searchbar.svelte";
   import { location } from "svelte-spa-router";
 
+  export let title: string = "";
+
   $: showSearchBar = $location === "/items" || $location === "/claimants";
 </script>
 
 <div class="flex min-h-screen bg-[#800000]">
   <Navbar />
   <main class="flex-1 overflow-auto p-7">
-    <div class="bg-white rounded-4xl p-6 h-full">
+    <div class="bg-white rounded-4xl p-7 h-full">
       {#if showSearchBar}
-        <Searchbar />
+        <div class="flex mb-8 justify-between">
+          <div class="p-2">
+            <h1 class={`text-5xl font-semibold`}>{title}</h1>
+          </div>
+          <div class="w-2/3 flex justify-end">
+            <Searchbar />
+          </div>
+        </div>
       {/if}
       <slot />
     </div>
