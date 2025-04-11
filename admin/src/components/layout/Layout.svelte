f4552c59c7a953deb0fd006eace14f5cdc246717
Fervicmar <fervicmardlagman@gmail.com>
2025-04-11 14:53:28 +0800

    Added search bar in the layout

--- admin/src/components/layout/Layout.svelte
+++ admin/src/components/layout/Layout.svelte
@@ -1,9 +1,17 @@
 <script lang="ts">
   import Navbar from "./Navbar.svelte";
+  import Searchbar from "../common/Searchbar.svelte";
+  import { location } from "svelte-spa-router";
+
+  $: showSearchBar = $location === "/items" || $location === "/claimants";
+
 </script>
 
 <div class="flex min-h-screen">
   <Navbar />
+  {#if showSearchBar}
+  <Searchbar />
+  {/if}
   <main class="flex-1 overflow-auto p-4">
     <slot />
   </main>
