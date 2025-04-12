ebde90e466b7d718201f3918831ae52f8a97a455
Henry James Carlos <henryjamescarlos@gmail.com>
2025-04-13 03:11:38 +0800

    Defined category and status options for filtering

---
+++ client/src/lib/constants/filters.ts
@@ -0,0 +1,17 @@
+export const categoryOptions = [
+  { value: "BA", label: "Bags & Backpacks", icon: "/vite.svg" },
+  { value: "EL", label: "Electronics", icon: "/vite.svg" },
+  { value: "EW", label: "Eyewear", icon: "/vite.svg" },
+  { value: "FW", label: "Footwear", icon: "/vite.svg" },
+  { value: "ID", label: "IDs & Cards", icon: "/vite.svg" },
+  { value: "KY", label: "Keys", icon: "/vite.svg" },
+  { value: "MS", label: "Miscellaneous", icon: "/vite.svg" },
+  { value: "MB", label: "Mobile Devices", icon: "/vite.svg" },
+  { value: "WT", label: "Wallets & Purses", icon: "/vite.svg" },
+  { value: "WH", label: "Watches & Jewelry", icon: "/vite.svg" }
+];
+
+export const statusOptions = [
+  { value: "UC", label: "Unclaimed", icon: "/vite.svg" },
+  { value: "CL", label: "Claimed", icon: "/vite.svg" }
+];
