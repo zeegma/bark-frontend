4315db559e0963d389d1d05a9621aed8f8e62190
Fervicmar <fervicmardlagman@gmail.com>
2025-04-14 01:42:45 +0800

    Created dropdown for category, claimants, and status

---
+++ admin/src/components/common/Category.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+  import { Button, Dropdown, DropdownItem } from "flowbite-svelte";
+  import { ChevronDownOutline } from "flowbite-svelte-icons";
+  export let mode: "add" | "edit" | "view";
+
+  $: isDisabled = mode === "view";
+
+  const categoryOptions = [
+    "Bags & Backpacks",
+    "Electronics",
+    "Eyewear",
+    "Footwear",
+    "IDs & Cards",
+    "Keys",
+    "Miscellaneous",
+    "Mobile Devices",
+    "Wallets & Purses",
+    "Watches & Jewelries",
+  ];
+
+  let selectedCategory: string = "";
+
+  function selectCategory(category: string) {
+    selectedCategory = category;
+  }
+</script>
+
+<div class="w-full">
+  <Button
+    disabled={isDisabled}
+    class="w-full bg-white border border-gray-300 text-[#1E1E1E] justify-between hover:bg-white hover:border-gray-300 focus:outline-none focus:ring-0 "
+    >{selectedCategory ? selectedCategory : "Category"}<ChevronDownOutline
+      class="w-6 h-6 ms-2 text-[#1E1E1E]"
+    /></Button
+  >
+  <Dropdown>
+    {#each categoryOptions as category}
+      <DropdownItem
+        on:click={() => selectCategory(category)}
+        disabled={isDisabled}
+        aria-required
+        >{category}
+      </DropdownItem>
+    {/each}
+  </Dropdown>
+</div>
---
+++ admin/src/components/common/Status.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { Button, Dropdown, DropdownItem } from "flowbite-svelte";
+  import { ChevronDownOutline } from "flowbite-svelte-icons";
+  export let mode: "add" | "edit" | "view";
+
+  $: isDisabled = mode === "view";
+
+  const statusOptions = ["Unclaimed", "Claimed"];
+  let selectedStatus: string = "";
+
+  function selectStatus(status: string) {
+    selectedStatus = status;
+  }
+</script>
+
+<div class="w-full">
+  <Button
+    disabled={isDisabled}
+    class="w-full bg-white border border-gray-300 text-[#1E1E1E] justify-between hover:bg-white hover:border-gray-300 focus:outline-none focus:ring-0 "
+    >{selectedStatus ? selectedStatus : "Status"}<ChevronDownOutline
+      class="w-6 h-6 ms-2 text-[#1E1E1E]"
+    /></Button
+  >
+  <Dropdown>
+    {#each statusOptions as status}
+      <DropdownItem
+        on:click={() => selectStatus(status)}
+        disabled={isDisabled}
+        aria-required
+        >{status}
+      </DropdownItem>
+    {/each}
+  </Dropdown>
+</div>
