410ed919678c30f540ec85f9ef96e7f4b5f7c26d
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-04-13 20:31:28 +0800

    Integrated table on claimant page

--- admin/src/routes/Claimants.svelte
+++ admin/src/routes/Claimants.svelte
@@ -3,15 +3,21 @@
   import SortDropdown from "../components/common/SortDropdown.svelte";
   import DatePicker from "../components/common/DatePicker.svelte";
   import Toggle from "../components/common/Toggle.svelte";
+  import Table from "../components/common/Table.svelte";
 </script>
 
 <Layout title="Claimants">
   <div class="p-2">
     <!-- Filter section -->
-    <div class="flex gap-4">
+    <div class="flex gap-4 mb-7">
       <SortDropdown />
       <DatePicker />
       <Toggle />
+    </div>
+
+    <!-- Table section -->
+    <div class="overflow-auto h-[calc(100vh-18rem)]">
+      <Table />
     </div>
   </div>
 </Layout>
