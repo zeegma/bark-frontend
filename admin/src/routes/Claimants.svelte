9db514cd828d5b9b8000aa5a63bcc7380f20ccc2
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-04-13 21:30:06 +0800

    Implemented toggling on claimants panel

--- admin/src/routes/Claimants.svelte
+++ admin/src/routes/Claimants.svelte
@@ -4,6 +4,15 @@
   import DatePicker from "../components/common/DatePicker.svelte";
   import Toggle from "../components/common/Toggle.svelte";
   import Table from "../components/common/Table.svelte";
+  import Grid from "../components/common/Grid.svelte";
+  import { viewStore, type ViewType } from "../stores/viewStore";
+
+  // Subscribe to the viewStore
+  let currentView: ViewType;
+
+  viewStore.subscribe((value) => {
+    currentView = value;
+  });
 </script>
 
 <Layout title="Claimants">
@@ -17,7 +26,11 @@
 
     <!-- Table section -->
     <div class="overflow-auto h-[calc(100vh-18rem)]">
-      <Table />
+      {#if currentView === "list"}
+        <Table />
+      {:else}
+        <Grid />
+      {/if}
     </div>
   </div>
 </Layout>
