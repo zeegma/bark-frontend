95836bad625c599979875ecb50cedfb3d9518728
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-04-13 18:12:47 +0800

    Added datepicker on filter section

--- admin/src/routes/Claimants.svelte
+++ admin/src/routes/Claimants.svelte
@@ -1,10 +1,15 @@
 <script lang="ts">
   import Layout from "../components/layout/Layout.svelte";
   import SortDropdown from "../components/common/SortDropdown.svelte";
+  import DatePicker from "../components/common/DatePicker.svelte";
 </script>
 
 <Layout title="Claimants">
   <div class="p-2">
-    <SortDropdown />
+    <!-- Filter section -->
+    <div class="flex gap-4">
+      <SortDropdown />
+      <DatePicker />
+    </div>
   </div>
 </Layout>
