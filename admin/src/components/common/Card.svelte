53ffbcaa859d25671f1ab4ed6c98ec5b5e53bd64
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-04-14 16:13:56 +0800

    Implemented new SVG component on card

--- admin/src/components/common/Card.svelte
+++ admin/src/components/common/Card.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { DotsVerticalOutline } from "flowbite-svelte-icons";
+  import Placeholder from "./Placeholder.svelte";
   import {
     selectionStore,
     selectionActions,
@@ -90,20 +91,7 @@
           <div
             class="w-12 h-12 bg-gray-300 flex items-center justify-center rounded"
           >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6 text-gray-500"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
-              />
-            </svg>
+            <Placeholder />
           </div>
           <span class="text-xs text-gray-500 mt-1">NO PHOTO AVAILABLE</span>
         </div>
