2c2ecd674036d3aa7933c88cf7df78fc18d9de61
Henry James Carlos <henryjamescarlos@gmail.com>
2025-04-14 12:56:52 +0800

    Replaced placeholder icon paths with actual icon paths

--- client/src/lib/constants/filters.ts
+++ client/src/lib/constants/filters.ts
@@ -1,17 +1,17 @@
 export const categoryOptions = [
-  { value: "BA", label: "Bags & Backpacks", icon: "/vite.svg" },
-  { value: "EL", label: "Electronics", icon: "/vite.svg" },
-  { value: "EW", label: "Eyewear", icon: "/vite.svg" },
-  { value: "FW", label: "Footwear", icon: "/vite.svg" },
-  { value: "ID", label: "IDs & Cards", icon: "/vite.svg" },
-  { value: "KY", label: "Keys", icon: "/vite.svg" },
-  { value: "MS", label: "Miscellaneous", icon: "/vite.svg" },
-  { value: "MB", label: "Mobile Devices", icon: "/vite.svg" },
-  { value: "WT", label: "Wallets & Purses", icon: "/vite.svg" },
-  { value: "WH", label: "Watches & Jewelry", icon: "/vite.svg" }
+  { value: "BA", label: "Bags & Backpacks", icon: "/icons/ct-bags.svg" },
+  { value: "EL", label: "Electronics", icon: "/icons/ct-electronics.svg" },
+  { value: "EW", label: "Eyewear", icon: "/icons/ct-eyewear.svg" },
+  { value: "FW", label: "Footwear", icon: "/icons/ct-footwear.svg" },
+  { value: "ID", label: "IDs & Cards", icon: "/icons/ct-cards.svg" },
+  { value: "KY", label: "Keys", icon: "/icons/ct-keys.svg" },
+  { value: "MS", label: "Miscellaneous", icon: "/icons/ct-misc.svg" },
+  { value: "MB", label: "Mobile Devices", icon: "/icons/ct-mobile.svg" },  
+  { value: "WT", label: "Wallets & Purses", icon: "/icons/ct-wallets.svg" },
+  { value: "WH", label: "Watches & Jewelry", icon: "/icons/ct-jewelry.svg" }
 ];
 
 export const statusOptions = [
-  { value: "UC", label: "Unclaimed", icon: "/vite.svg" },
-  { value: "CL", label: "Claimed", icon: "/vite.svg" }
+  { value: "UC", label: "Unclaimed", icon: "/icons/st-unclaimed.svg" },
+  { value: "CL", label: "Claimed", icon: "/icons/st-claimed.svg" }
 ];
