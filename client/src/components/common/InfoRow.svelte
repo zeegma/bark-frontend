9d6ac1af443f2a0a83a0b8e6e12c9c884fa8cfa9
Henry James Carlos <henryjamescarlos@gmail.com>
2025-04-15 18:12:26 +0800

    Created a component for item desc, date, and time

---
+++ client/src/components/common/InfoRow.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let label: string;
+  export let value: string;
+</script>
+
+<div class="flex border border-stone-300 rounded-lg px-5 py-3">
+  <p class="font-medium text-stone-400">
+    <span class="text-stone-600">{label}:</span>
+    {value}
+  </p>
+</div>
