edd51273f658f92bfbef169e80b505523baa9672
Henry James Carlos <henryjamescarlos@gmail.com>
2025-04-15 16:39:00 +0800

    Implemented item details fetching

--- client/src/routes/Claim.svelte
+++ client/src/routes/Claim.svelte
@@ -1,6 +1,38 @@
 <script lang="ts">
+  import Header from "../components/layout/Header.svelte";
+  import { onMount } from "svelte";
+  import { getQueryParameter } from "../lib/utils/queryParser";
+  import { fetchItemDetails } from "../lib/api/fetch";
+
+  let itemId: string | null = null;
+  let itemDetails: any = null;
+  let isLoading: boolean = true;
+
+  onMount(() => {
+    itemId = getQueryParameter("id");
+    if (itemId) {
+      fetchItemDetails(itemId)
+        .then((data) => {
+          itemDetails = data;
+        })
+        .catch((error) => {
+          console.error("Error fetching item details:", error);
+        })
+        .finally(() => {
+          isLoading = false;
+        });
+    } else {
+      isLoading = false;
+    }
+  });
 </script>
 
-<div class="text-3xl">
-    Claim Request
-</div>
+<Header />
+
+{#if isLoading}
+  <p>Loading...</p>
+{:else if !itemDetails}
+  <p>Item not found.</p>
+{:else}
+  <p>Item details and Claim Form</p>
+{/if}
