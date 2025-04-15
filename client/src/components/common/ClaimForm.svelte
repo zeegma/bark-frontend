c791d9d89eda7c10dc28a47abd0ac23755229f86
Henry James Carlos <henryjamescarlos@gmail.com>
2025-04-15 19:48:37 +0800

    Created an initial structure for claim form

---
+++ client/src/components/common/ClaimForm.svelte
@@ -0,0 +1,54 @@
+<script lang="ts"></script>
+
+<div class="">
+  <form action="" class="flex flex-col gap-4">
+    <label
+      >Full Name
+      <input
+        type="text"
+        name="name"
+        required
+        class="border border-gray-300 rounded-md p-2"
+      />
+    </label>
+    <label
+      >Contact Number
+      <input
+        type="tel"
+        name="contact_number"
+        required
+        class="border border-gray-300 rounded-md p-2"
+      />
+    </label>
+    <label
+      >Facebook Link
+      <input
+        type="url"
+        name="facebook_link"
+        required
+        class="border border-gray-300 rounded-md p-2"
+      />
+    </label>
+    <label>
+      Detailed Description
+      <textarea
+        name="description"
+        required
+        class="border border-gray-300 rounded-md p-2"
+      ></textarea>
+    </label>
+    <label>
+      Ownership Photo
+      <input
+        type="file"
+        name="photo"
+        accept="image/*"
+        required
+        class="border border-gray-300 rounded-md p-2"
+      />
+    </label>
+    <button type="submit" class="bg-[#800000] text-white rounded-md p-2">
+      Submit Claim
+    </button>
+  </form>
+</div>
