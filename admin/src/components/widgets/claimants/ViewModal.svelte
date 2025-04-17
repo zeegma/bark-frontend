5ce126bf4a8caf318f8101998d5f5f9deeac4668
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-04-17 19:31:25 +0800

    Added description & updated to match API structure

--- admin/src/components/widgets/claimants/ViewModal.svelte
+++ admin/src/components/widgets/claimants/ViewModal.svelte
@@ -1,20 +1,11 @@
 <script lang="ts">
-  import { Modal, Label, Input } from "flowbite-svelte";
+  import { Modal, Label, Input, Textarea } from "flowbite-svelte";
   import DatePicker from "../../common/DatePicker.svelte";
   import Placeholder from "../../common/Placeholder.svelte";
+  import { type ClaimItem } from "../../../lib/types";
 
   export let open = false;
-
-  export let claim: {
-    id: string;
-    name: string;
-    phone: string;
-    facebook: string;
-    dateFiled: string;
-    itemId: string;
-    itemRequested: string;
-    hasImage?: boolean;
-  } | null;
+  export let claim: ClaimItem | null;
 </script>
 
 {#if claim}
@@ -23,13 +14,12 @@
     autoclose
     outsideclose
     size="lg"
-    placement="center-right"
-    class="bg-gray-50 rounded-3xl ring-1 ring-gray-800 right-40"
+    class="bg-gray-50 rounded-3xl ring-1 ring-gray-800"
   >
     <div class="px-2 pb-5">
       <div class="flex justify-between items-start mb-4">
         <h3 class="text-4xl font-bold text-gray-900 dark:text-white">
-          {claim.id}
+          CLAIMANT #{claim.id}
         </h3>
       </div>
 
@@ -96,20 +86,20 @@
         <div class="flex flex-col h-full">
           <div class="grid grid-cols-2 gap-4 flex-grow">
             <!-- Proof photo -->
-            <div class="space-y-2 flex flex-col">
+            <div class="space-y-2 flex flex-col h-full">
               <Label class="text-sm font-medium">Ownership Proof Image</Label>
               <div
                 class="border-2 border-dashed border-gray-300 flex items-center justify-center flex-grow bg-white rounded-xl"
               >
                 {#if claim.hasImage}
                   <img
-                    src="src/assets/me.png"
+                    src={claim.ownershipProofPhoto}
                     alt="Ownership Proof"
                     class="w-40 h-40 object-cover"
                   />
                 {:else}
                   <Placeholder />
-                  <span class="text-xs text-gray-500 mt-1"
+                  <span class="text-xs text-gray-500 my-16 py-2"
                     >NO PHOTO AVAILABLE</span
                   >
                 {/if}
@@ -117,24 +107,38 @@
             </div>
 
             <!-- Lost photo -->
-            <div class="space-y-2 flex flex-col">
+            <div class="space-y-2 flex flex-col h-full">
               <Label class="text-sm font-medium">Lost Item Image</Label>
               <div
-                class="border-2 border-dashed border-gray-300 flex items-center justify-center flex-grow bg-white rounded-xl"
+                class="border-2 border-dashed border-gray-300 flex items-center justify-center flex-grow bg-white rounded-xl h-full"
               >
-                {#if claim.hasImage}
+                {#if claim.itemPhoto}
                   <img
-                    src="src/assets/lost.jpg"
+                    src={claim.itemPhoto}
                     alt="Lost Item"
                     class="w-40 h-40 object-cover"
                   />
                 {:else}
                   <Placeholder />
-                  <span class="text-xs text-gray-500 mt-1"
+                  <span class="text-xs text-gray-500 my-16 py-2"
                     >NO PHOTO AVAILABLE</span
                   >
                 {/if}
               </div>
+            </div>
+
+            <!-- Description -->
+            <div class="space-y-2 col-span-2 flex flex-col h-full">
+              <Label for="description" class="text-sm font-medium"
+                >Description</Label
+              >
+              <Textarea
+                id="description"
+                rows={3}
+                value={claim.detailedDescription}
+                readonly
+                class="bg-white dark:bg-gray-800 text-gray-600 h-full"
+              />
             </div>
           </div>
         </div>
