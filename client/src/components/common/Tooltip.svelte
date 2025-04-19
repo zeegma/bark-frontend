80ab6ad740ae1b0485a9f3b840b30ecbb19e3698
Henry James Carlos <henryjamescarlos@gmail.com>
2025-04-19 23:33:24 +0800

    Modified tooltip component to handle bottom positioning

--- client/src/components/common/Tooltip.svelte
+++ client/src/components/common/Tooltip.svelte
@@ -1,17 +1,44 @@
 <script lang="ts">
+  import { onClickOutside } from "../../lib/utils/onClickOutside";
+
   export let text: string;
-  export let position: "top" | "left" = "top";
+  export let position: "top" | "left" | "bottom" = "top";
+
+  let showTooltip = false;
+  function toggleTooltip() {
+    showTooltip = !showTooltip;
+  }
+
+  function closeTooltip() {
+    showTooltip = false;
+  }
 </script>
 
-<div class="group relative">
+<div
+  class="group relative"
+  role="button"
+  tabindex="0"
+  on:click={toggleTooltip}
+  on:keydown={(e) =>
+    e.key === "Enter" || e.key === " " ? toggleTooltip() : null}
+  use:onClickOutside={closeTooltip}
+>
   <slot />
   <!-- Tooltip -->
   <div
-    class={`absolute z-10 px-6 py-6 bg-[#800000] rounded-lg opacity-0 group-hover:opacity-100 transition-opacity duration-300 ${
+    class={`absolute z-10 p-4 md:p-6 bg-[#800000] rounded-lg ${
+      showTooltip
+        ? "opacity-100 pointer-events-auto"
+        : "opacity-0 pointer-events-none"
+    } transition-opacity duration-300 ${
       position === "top"
-        ? "w-full bottom-full mb-4 left-1/2 transform -translate-x-1/2"
-        : "w-72 right-54 text-left top-1/2 transform -translate-y-1/2"
-    } pointer-events-none group-hover:pointer-events-auto shadow-xl shadow-stone-800/50`}
+        ? "w-46 sm:w-full bottom-full mb-4 left-1/2 transform -translate-x-1/2"
+        : position === "left"
+          ? "w-72 right-full mr-4 top-1/2 transform -translate-y-1/2"
+          : position === "bottom"
+            ? "w-42 top-full mt-3 right-0"
+            : ""
+    } shadow-xl shadow-stone-800/50`}
   >
     {@html text}
     <!-- Safe Area -->
@@ -19,15 +46,23 @@
       class={`${
         position === "top"
           ? "absolute w-full h-8 left-1/2 transform -translate-x-1/2 translate-y-4"
-          : "absolute h-1/2 top-0 left-full w-8 transform translate-y-1/2"
+          : position === "left"
+            ? "absolute h-1/2 top-0 left-full w-8 transform translate-y-1/2"
+            : position === "bottom"
+              ? "hidden"
+              : ""
       }`}
     ></div>
     <!-- Arrow -->
     <div
       class={`absolute w-0 h-0 ${
         position === "top"
-          ? "left-1/2 bottom-0 border-l-[6px] border-r-[6px] border-t-[6px] border-l-transparent border-r-transparent border-t-[#800000] transform - translate-x-24 translate-y-full"
-          : "left-full border-t-[6px] border-b-[6px] border-l-[6px] border-l-[#800000] border-b-transparent border-t-transparent transform -translate-y-16"
+          ? "right-16 bottom-0 border-l-[6px] border-r-[6px] border-t-[6px] border-l-transparent border-r-transparent border-t-[#800000] transform translate-y-full"
+          : position === "left"
+            ? "left-full border-t-[6px] border-b-[6px] border-l-[6px] border-l-[#800000] border-b-transparent border-t-transparent transform -translate-y-16"
+            : position === "bottom"
+              ? "bottom-full right-0 -translate-x-2 translate-y-0 border-l-[6px] border-r-[6px] border-b-[6px] border-l-transparent border-r-transparent border-b-[#800000]"
+              : ""
       }`}
     ></div>
   </div>
