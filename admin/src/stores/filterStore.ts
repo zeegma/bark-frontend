843b1c6d739881b1ce30fb9c6338523b1c868dd0
Fervicmar <fervicmardlagman@gmail.com>
2025-04-22 00:39:43 +0800

    Added categoryMap and statusMap

--- admin/src/stores/filterStore.ts
+++ admin/src/stores/filterStore.ts
@@ -33,3 +33,30 @@ export const filterStore = writable<FilterOptions>({
 
 export type CategoryFilter = (typeof categoryOptions)[number];
 export type StatusFilter = (typeof statusOptions)[number];
+
+export const categoryMap: Record<string, string> = {
+  "BAGS_&_BACKPACKS": "BA",
+  ELECTRONICS: "EL",
+  EYEWEAR: "EW",
+  FOOTWEAR: "FW",
+  "IDS_&_CARDS": "ID",
+  KEYS: "KY",
+  MISCELLANEOUS: "MS",
+  MOBILE_DEVICES: "MB",
+  "WALLETS_&_PURSES": "WT",
+  "WATCHES_&_JEWELRIES": "WH",
+};
+
+export const statusMap: Record<string, string> = {
+  UNCLAIMED: "UC",
+  CLAIMED: "CL",
+  EXPIRED: "EX",
+};
+
+export const reverseCategoryMap = Object.fromEntries(
+  Object.entries(categoryMap).map(([key, val]) => [val, key]),
+);
+
+export const reverseStatusMap = Object.fromEntries(
+  Object.entries(statusMap).map(([key, val]) => [val, key]),
+);
