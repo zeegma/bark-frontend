0f963313df009179d96247ff20a0e22f7370fe7c
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-04-29 21:40:40 +0800

    Modified empty fallback to handle two scenarios

--- admin/src/components/common/EmptyFallback.svelte
+++ admin/src/components/common/EmptyFallback.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
   import { UsersGroupSolid } from "flowbite-svelte-icons";
+
+  let { message, subMessage } = $props();
 </script>
 
 <div class="flex flex-col justify-center items-center h-full p-8 text-gray-800">
   <UsersGroupSolid class="w-16 h-16 mb-4 text-red-500" />
-  <p class="text-lg font-semibold">No claimants at the moment.</p>
+  <p class="text-lg font-semibold">{message}</p>
   <p class="text-sm text-gray-400">
-    Looks like nobody has requested a claim yet.
+    {subMessage}
   </p>
 </div>
