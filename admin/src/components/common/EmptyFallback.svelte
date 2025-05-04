97462c0b85c509c3f8c86dffc3efc7b22fbb96a8
Fervicmar <fervicmardlagman@gmail.com>
2025-05-05 01:51:16 +0800

    Updated EmptyFallback.svelte to make it applicable for items

--- admin/src/components/common/EmptyFallback.svelte
+++ admin/src/components/common/EmptyFallback.svelte
@@ -1,11 +1,21 @@
 <script lang="ts">
-  import { UsersGroupSolid } from "flowbite-svelte-icons";
+  import { UsersGroupSolid, DropboxSolid } from "flowbite-svelte-icons";
+
+  export let type: "items" | "claimants" = "items";
 </script>
 
 <div class="flex flex-col justify-center items-center h-full p-8 text-gray-800">
-  <UsersGroupSolid class="w-16 h-16 mb-4 text-red-500" />
-  <p class="text-lg font-semibold">No claimants at the moment.</p>
-  <p class="text-sm text-gray-400">
-    Looks like nobody has requested a claim yet.
-  </p>
+  {#if type === "claimants"}
+    <UsersGroupSolid class="w-16 h-16 mb-4 text-red-500" />
+    <p class="text-lg font-semibold">No claimants at the moment.</p>
+    <p class="text-sm text-gray-400">
+      Looks like nobody has requested a claim yet.
+    </p>
+  {:else}
+    <DropboxSolid class="w-16 h-16 mb-4 text-red-500" />
+    <p class="text-lg font-semibold">No items at the moment.</p>
+    <p class="text-sm text-gray-400">
+      Looks like nobody has reported a lost item yet.
+    </p>
+  {/if}
 </div>
