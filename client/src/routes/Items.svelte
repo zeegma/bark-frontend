92743fbaa71ed1793f16f377de60fe4729875182
Henry James Carlos <henryjamescarlos@gmail.com>
2025-06-16 15:04:49 +0800

    Fix footer positioning

--- client/src/routes/Items.svelte
+++ client/src/routes/Items.svelte
@@ -75,37 +75,41 @@
 </script>
 
 <Header />
-{#if isLoading}
-  {#if view === "grid"}
-    <GridLoader />
-  {:else}
-    <ListLoader />
-  {/if}
-{:else}
-  <Filters bind:view />
-  {#if filteredItems.length === 0}
-    <!-- Empty State -->
-    <div class="flex flex-col items-center justify-center h-88 px-8">
-      <img
-        src="/icons/nobark.svg"
-        alt=""
-        class="w-36 sm:w-48 md:w-72 mb-6 md:mb-8"
-      />
-      <h1
-        class="font-[Poppins] font-medium text-center text-base sm:text-xl md:text-2xl text-[#800000] mb-2 md:mb-3"
-      >
-        No Items Found...
-      </h1>
-      <p
-        class="font-[Poppins] font-medium text-xs sm:text-sm md:text-md text-center text-red-400 max-w-64 md:max-w-96"
-      >
-        We couldn’t find anything. Try adjusting your filters or refreshing the
-        page if the problem persists.
-      </p>
-    </div>
-  {:else}
-    <Contents items={filteredItems} {view} />
-  {/if}
-{/if}
 
-<Footer />
+<div class="flex flex-col justify-between h-screen">
+  <div>
+    {#if isLoading}
+      {#if view === "grid"}
+        <GridLoader />
+      {:else}
+        <ListLoader />
+      {/if}
+    {:else}
+      <Filters bind:view />
+      {#if filteredItems.length === 0}
+        <!-- Empty State -->
+        <div class="flex flex-col items-center justify-center h-88 px-8">
+          <img
+            src="/icons/nobark.svg"
+            alt=""
+            class="w-36 sm:w-48 md:w-72 mb-6 md:mb-8"
+          />
+          <h1
+            class="font-[Poppins] font-medium text-center text-base sm:text-xl md:text-2xl text-[#800000] mb-2 md:mb-3"
+          >
+            No Items Found...
+          </h1>
+          <p
+            class="font-[Poppins] font-medium text-xs sm:text-sm md:text-md text-center text-red-400 max-w-64 md:max-w-96"
+          >
+            We couldn’t find anything. Try adjusting your filters or refreshing
+            the page if the problem persists.
+          </p>
+        </div>
+      {:else}
+        <Contents items={filteredItems} {view} />
+      {/if}
+    {/if}
+  </div>
+  <Footer />
+</div>
