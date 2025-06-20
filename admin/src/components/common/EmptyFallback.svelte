714eb153bfa6683a0823bee03cd58499d1ca395b
Fervicmar <fervicmardlagman@gmail.com>
2025-06-20 11:07:55 +0800

    Updated EmptyFallback.svelte

--- admin/src/components/common/EmptyFallback.svelte
+++ admin/src/components/common/EmptyFallback.svelte
@@ -2,20 +2,21 @@
   import { UsersGroupSolid, DropboxSolid } from "flowbite-svelte-icons";
 
   export let type: "items" | "claimants" = "items";
+  export let message: string | null = null;
+  export let subMessage: string | null = null;
 </script>
 
 <div class="flex flex-col justify-center items-center h-full p-8 text-gray-800">
   {#if type === "claimants"}
     <UsersGroupSolid class="w-16 h-16 mb-4 text-red-500" />
-    <p class="text-lg font-semibold">No claimants at the moment.</p>
-    <p class="text-sm text-gray-400">
-      Looks like nobody has requested a claim yet.
-    </p>
   {:else}
     <DropboxSolid class="w-16 h-16 mb-4 text-red-500" />
-    <p class="text-lg font-semibold">No items at the moment.</p>
-    <p class="text-sm text-gray-400">
-      Looks like nobody has reported a lost item yet.
-    </p>
+  {/if}
+
+  {#if message}
+    <p class="text-lg font-semibold">{message}</p>
+  {/if}
+  {#if subMessage}
+    <p class="text-sm text-gray-400">{subMessage}</p>
   {/if}
 </div>
