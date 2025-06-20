5551408129d3a50d4911fd968495dc26183211da
Fervicmar <fervicmardlagman@gmail.com>
2025-06-20 10:55:33 +0800

    Removed unnecessary imports

--- admin/src/App.svelte
+++ admin/src/App.svelte
@@ -1,9 +1,6 @@
 <script lang="ts">
   import Router from "svelte-spa-router";
   import Login from "./routes/Login.svelte";
-  import Dashboard from "./routes/Dashboard.svelte";
-  import Items from "./routes/Items.svelte";
-  import Claimants from "./routes/Claimants.svelte";
   import Fallback from "./routes/Fallback.svelte";
   import Register from "./routes/Register.svelte";
   import Unauthorized from "./routes/Unauthorized.svelte";
