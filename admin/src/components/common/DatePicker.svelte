1e67268cb76c0d14fe1ee536b05ba2fe5c16eadf
Acelle Krislette Rosales <acellekrislette@gmail.com>
2025-06-20 13:35:18 +0800

    Created optional argument for datepicker and restored ranged func

--- admin/src/components/common/DatePicker.svelte
+++ admin/src/components/common/DatePicker.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
   import { Datepicker } from "flowbite-svelte";
-  import { dateFilterActions } from "../../stores/dateFilterStore";
 
   export let disabled: boolean = false;
   export let value: Date | null = null;
   export let selectedDate: Date | null = value;
   export let ranged: boolean = false;
+
+  // Optional injection of actions
+  export let setDateRange:
+    | ((start: Date | null, end: Date | null) => void)
+    | null = null;
+  export let clearDateFilter: (() => void) | null = null;
 
   let lastAction: string = "";
   let dateRange = { from: null, to: null };
@@ -15,29 +20,48 @@
   function handleClear(): void {
     lastAction = "Cleared";
     selectedDate = null;
-    dateFilterActions.clearDateFilter();
+    clearDateFilter?.();
   }
 
   function handleApply(event: CustomEvent<Date>): void {
     lastAction = "Applied";
     selectedDate = event.detail;
 
+    // Do nothing if not in filter mode
+    if (!setDateRange) return;
+
     ranged
-      ? dateFilterActions.setDateRange(dateRange.from, dateRange.to)
-      : dateFilterActions.setDateRange(event.detail, event.detail);
+      ? setDateRange(dateRange.from, dateRange.to)
+      : setDateRange(event.detail, event.detail);
   }
 </script>
 
 <div class="w-full">
-  <Datepicker
-    bind:value={selectedDate}
-    on:clear={handleClear}
-    on:apply={handleApply}
-    {disabled}
-    color="red"
-    showActionButtons
-    placeholder="Date"
-    autohide={false}
-    inputClass="border border-gray-300 bg-white text-[#1E1E1E] rounded-xl h-11"
-  />
+  {#if ranged}
+    <Datepicker
+      range
+      bind:rangeFrom={dateRange.from}
+      bind:rangeTo={dateRange.to}
+      on:clear={handleClear}
+      on:apply={handleApply}
+      {disabled}
+      color="red"
+      showActionButtons
+      placeholder="Date"
+      autohide={false}
+      inputClass="border border-gray-300 bg-white text-gray-600 hover:bg-gray-100 rounded-xl h-11"
+    />
+  {:else}
+    <Datepicker
+      bind:value={selectedDate}
+      on:clear={handleClear}
+      on:apply={handleApply}
+      {disabled}
+      color="red"
+      showActionButtons
+      placeholder="Date"
+      autohide={false}
+      inputClass="border border-gray-300 bg-white text-gray-600 hover:bg-gray-100 rounded-xl h-11"
+    />
+  {/if}
 </div>
